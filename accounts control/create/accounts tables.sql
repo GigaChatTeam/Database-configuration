46e48b216ccf4276464db59c4df3a9b77fd001e2
mister-proger <84221561+mister-proger@users.noreply.github.com>
2023-09-11 01:07:55 +0300

    Commands have been created to create tables associated with user accounts.

---
+++ accounts control/create/accounts tables.sql
@@ -0,0 +1,27 @@
+CREATE TABLE public.accounts (
+    id BIGSERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    confirmation INTEGER DEFAULT 0,
+    password TEXT NOT NULL,
+    email TEXT UNIQUE,
+    emails TEXT[],
+    phone TEXT UNIQUE,
+    created TIMESTAMP NOT NULL,
+    deleted BOOLEAN DEFAULT FALSE
+)
+
+CREATE TABLE public.accounts_changes (
+    client BIGINT PRIMARY KEY,
+    nickname TIMESTAMP[],
+    password TIMESTAMP[],
+    FOREIGN KEY (client) REFERENCES public.accounts (id)
+)
+
+CREATE TABLE public.profiles (
+    client INTEGER NOT NULL,
+    username TEXT NOT NULL,
+    nickname TEXT,
+    avatar BYTEA,
+    FOREIGN KEY (client) REFERENCES public.accounts (id),
+    FOREIGN KEY (username) REFERENCES public.accounts (username)
+)
---
+++ accounts control/create/tokens tables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE public.tokens (
+    client BIGINT NOT NULL,
+    agent TEXT NOT NULL,
+    token TEXT NOT NULL,
+    last_login TIMESTAMP,
+    start TIMESTAMP NOT NULL,
+    ending TIMESTAMP,
+    FOREIGN KEY (client) REFERENCES public.accounts (id)
+)
+
+CREATE TABLE public.ttokens (
+    client BIGINT NOT NULL,
+    token TEXT NOT NULL,
+    hashed BOOL NOT NULL DEFAULT FALSE,
+    extradition TIMESTAMP NOT NULL,
+    intentions TEXT[] NOT NULL,
+    blocked TIMESTAMP,
+    reason TEXT,
+    comments TEXT[],
+    FOREIGN KEY (client) REFERENCES public.accounts (id)
+)
