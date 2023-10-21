dc1ce75a2339c8479614359949b866394dee397c
mister-proger <i@savnil.ru>
2023-10-21 14:10:55 +0300

    Update function for validate time-tokens

--- users control/functions/tokens.sql
+++ users control/functions/tokens.sql
@@ -1,32 +1,30 @@
-CREATE FUNCTION public.validate_ttoken (sourse_client BIGINT, sourse_token TEXT, sourse_intentions TEXT[])
+CREATE OR replace FUNCTION public.validate_ttoken (sourse_client BIGINT, sourse_token TEXT, sourse_intentions TEXT[])
 RETURNS BOOLEAN AS $$
-DECLARE
-    token RECORD;
 BEGIN
-    FOR token IN (
+    IF EXISTS (
         SELECT *
         FROM public.ttokens
         WHERE
             client = sourse_client AND
             intentions = sourse_intentions AND
+            token = sourse_token AND
             blocked IS NULL
         ORDER BY
             extradition ASC
-    ) LOOP
-        IF
-            token.token = sourse_token
-        THEN
-            UPDATE public.ttokens
-            SET
-                blocked = now(),
-                reason = 'TOKEN WAS USED'
-            WHERE
-                extradition = token.extradition AND
-                client = token.client;
+    ) THEN
+        UPDATE public.ttokens
+        SET
+            blocked = now(),
+            reason = 'TOKEN WAS USED'
+        WHERE
+            client = sourse_client AND
+            intentions = sourse_intentions AND
+            token = sourse_token AND
+            blocked IS NULL;
 
-            RETURN TRUE;
-        END IF;
-    END LOOP;
-    RETURN FALSE;
+        RETURN TRUE;
+    ELSE
+        RETURN FALSE;
+    END IF;
 END
 $$ LANGUAGE plpgsql;
