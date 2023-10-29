279866ddd0387e9211ee2a8bb51d790be62023b4
mister-proger <i@savnil.ru>
2023-10-29 12:26:08 +0300

    Added system for calculating occupied space of file servers

--- data threads control/files/setup.sql
+++ data threads control/files/setup.sql
@@ -16,13 +16,37 @@ CREATE TABLE attachments.files (
     storage UUID NOT NULL,
     intentions TEXT[],
     size BIGINT,
-    format TEXT NOT NULL,
+    format TEXT,
     filename TEXT NOT NULL,
     status TEXT NOT NULL DEFAULT 'EXPECTED',
     FOREIGN KEY (owner) REFERENCES public."accounts" (id),
     FOREIGN KEY (bot) REFERENCES public.bots (client),
     FOREIGN KEY (storage) REFERENCES servers.files (id)
 );
+
+CREATE FUNCTION attachments.update_occupied()
+RETURNS TRIGGER AS
+$$
+BEGIN
+    UPDATE servers.files
+    SET occupied = (
+        SELECT SUM(size)
+        FROM attachments.files
+        WHERE
+            status = 'STORED' AND
+            storage = NEW.storage
+    )
+    WHERE
+        id = NEW.storage;
+    RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER update_occupied
+AFTER INSERT OR UPDATE ON attachments.files
+FOR EACH ROW
+EXECUTE FUNCTION attachments.update_occupied();
 
 CREATE TABLE attachments.widgets (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
