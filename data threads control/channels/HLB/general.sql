565df4707c19174cff4ecde844fe7b863a867f72
mister-proger <i@savnil.ru>
2023-11-11 13:45:14 +0300

    Updated user channels selection function

--- data threads control/channels/HLB/general.sql
+++ data threads control/channels/HLB/general.sql
@@ -1,13 +1,18 @@
-CREATE FUNCTION channels.select_channels (sourse_client BIGINT, ttoken TEXT)
+CREATE OR REPLACE FUNCTION channels.select_channels (sourse_client BIGINT, ttoken TEXT)
 RETURNS SETOF BIGINT AS $$
 BEGIN
     IF public.validate_ttoken(sourse_client, ttoken, ARRAY ['LOAD', 'CHANNELS'])
     THEN
-        RETURN QUERY SELECT DISTINCT channel
-        FROM channels.users
-        WHERE
-            client = sourse_client AND
-            leaved IS NULL;
+        RETURN QUERY
+            SELECT DISTINCT channel
+            FROM channels.users
+            WHERE
+                client = sourse_client AND
+                leaved IS NULL;
+        IF NOT FOUND
+        THEN
+           RETURN QUERY SELECT 0::BIGINT;
+        END IF;
     ELSE
         RETURN;
     END IF;
