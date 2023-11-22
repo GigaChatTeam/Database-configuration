06995a4da6b6675201e8a423cc419a8b1fc227ed
mister-proger <i@savnil.ru>
2023-11-22 21:35:22 +0300

    Attachments and groups systems have been added, and type of stored link to a channel has been changed.

--- data threads control/channels/setup.sql
+++ data threads control/channels/setup.sql
@@ -30,11 +30,33 @@ CREATE TABLE channels.messages_data (
     original TIMESTAMP NOT NULL,
     edited TIMESTAMP NOT NULL,
     data TEXT,
-    attachments JSONB,
     version SMALLINT NOT NULL,
     PRIMARY KEY (channel, original, version),
     FOREIGN KEY (channel, original) REFERENCES channels.messages (channel, posted)
 ) PARTITION BY RANGE (original);
+
+CREATE TABLE channels.messages_attachments_media (
+    file BIGINT NOT NULL,
+    channel BIGINT NOT NULL,
+    original TIMESTAMP NOT NULL,
+    x SMALLINT[2] NOT NULL,
+    y SMALLINT[2] NOT NULL,
+    version SMALLINT NOT NULL DEFAULT 1,
+    PRIMARY KEY (file, channel, original, version),
+    FOREIGN KEY (file) REFERENCES attachments.files (id),
+    FOREIGN KEY (channel, original, version) REFERENCES channels.messages_data (channel, original, version)
+);
+
+CREATE TABLE channels.messages_attachments_audio (
+    file BIGINT NOT NULL,
+    channel BIGINT NOT NULL,
+    original TIMESTAMP NOT NULL,
+    position SMALLINT NOT NULL,
+    version SMALLINT NOT NULL DEFAULT 1,
+    PRIMARY KEY (file, channel, original, version),
+    FOREIGN KEY (file) REFERENCES attachments.files (id),
+    FOREIGN KEY (channel, original, version) REFERENCES channels.messages_data (channel, original, version)
+);
 
 CREATE TABLE channels.users (
     client BIGINT NOT NULL,
@@ -47,10 +69,28 @@ CREATE TABLE channels.users (
     FOREIGN KEY (channel) REFERENCES channels.index (id)
 );
 
-CREATE TABLE channels.permissions (
+CREATE TABLE channels.groups (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    channel BIGINT NOT NULL,
+    created TIMESTAMP NOT NULL,
+    created_by BIGINT NOT NULL,
+    title TEXT NOT NULL,
+    primacy SMALLINT NOT NULL,
+    FOREIGN KEY (created_by) REFERENCES users.accounts (id),
+    FOREIGN KEY (channel) REFERENCES channels.index (id)
+);
+
+CREATE TABLE channels.users_groups (
+    client BIGINT NOT NULL,
+    "group" BIGINT NOT NULL,
+    FOREIGN KEY (client) REFERENCES users.accounts (id),
+    FOREIGN KEY ("group") REFERENCES channels."groups" (id) ON DELETE CASCADE
+);
+
+CREATE TABLE channels.permissions_users (
     client BIGINT NOT NULL,
     channel BIGINT NOT NULL,
-    permission SMALLINT[4],
+    permission SMALLINT[4] NOT NULL,
     status BOOLEAN,
     PRIMARY KEY (client, channel, permission),
     FOREIGN KEY (client) REFERENCES users.accounts (id),
@@ -58,18 +98,27 @@ CREATE TABLE channels.permissions (
     FOREIGN KEY (permission) REFERENCES public.permissions (id)
 );
 
+CREATE TABLE channels.permissions_groups (
+    "group" BIGINT NOT NULL,
+    channel BIGINT NOT NULL,
+    permission SMALLINT[4] NOT NULL,
+    status BOOLEAN,
+    PRIMARY KEY ("group", channel, permission),
+    FOREIGN KEY ("group") REFERENCES channels."groups" (id) ON DELETE CASCADE,
+    FOREIGN KEY (channel) REFERENCES channels.index (id),
+    FOREIGN KEY (permission) REFERENCES public.permissions (id)
+);
+
 CREATE TABLE channels.invitations (
     creator BIGINT NOT NULL,
     channel BIGINT NOT NULL,
-    uri CHAR(16) DEFAULT substring(md5(public.uuid_generate_v4()::text), 0, 17),
+    uri TEXT DEFAULT substring(md5(public.uuid_generate_v4()::text), 0, 17) PRIMARY KEY,
     created TIMESTAMP NOT NULL DEFAULT now(),
     expiration TIMESTAMP,
     permitted_uses INTEGER,
     total_uses INTEGER NOT NULL DEFAULT 0,
     enabled BOOLEAN NOT NULL DEFAULT TRUE,
-    PRIMARY KEY (uri),
     FOREIGN KEY (creator) REFERENCES users.accounts (id),
     FOREIGN KEY (channel) REFERENCES channels.index (id),
-    CHECK (COALESCE(permitted_uses, 'Infinity'::NUMERIC) > total_uses),
-    CHECK (COALESCE(expiration, now()) > created)
+    CHECK (COALESCE(permitted_uses, 'Infinity'::NUMERIC) > total_uses)
 );
