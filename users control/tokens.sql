baa16ce9b2e453c37005d87d429aab5c0f11cee2
mister-proger <i@savnil.ru>
2023-12-15 18:29:32 +0300

    Update TSWTZ to TSWOTZ on tokens.sql

--- users control/tokens.sql
+++ users control/tokens.sql
@@ -3,7 +3,7 @@ CREATE TABLE users.tokens (
     agent TEXT NOT NULL,
     secret TEXT NOT NULL,
     key TEXT NOT NULL,
-    start TIMESTAMP NOT NULL DEFAULT now(),
+    start TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT TIMEZONE('UTC', now()),
     PRIMARY KEY (client, key),
     FOREIGN KEY (client) REFERENCES users.accounts ("id")
 );
@@ -11,8 +11,8 @@ CREATE TABLE users.tokens (
 CREATE TABLE users.logins (
     client BIGINT NOT NULL,
     key TEXT NOT NULL,
-    login TIMESTAMP NOT NULL,
-    duration INTERVAL,
+    login TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT TIMEZONE('UTC', now()),
+    exit TIMESTAMP WITHOUT TIME ZONE,
     agent TEXT,
     successfully BOOLEAN NOT NULL DEFAULT TRUE,
     FOREIGN KEY (client) REFERENCES users.accounts ("id")
