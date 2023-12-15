2e5e61f7f95a9d49f46c4c4c59ee696fe3a61730
mister-proger <i@savnil.ru>
2023-12-15 12:54:51 +0300

    Moved columns in attachments scheme to types without time zones

--- data threads control/files/setup.sql
+++ data threads control/files/setup.sql
@@ -1,50 +1,25 @@
 CREATE TABLE attachments.files (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    owner BIGINT NOT NULL,
+    "id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "owner" BIGINT NOT NULL,
     bot BIGINT,
-    upload TIMESTAMP NOT NULL DEFAULT now(),
-    intentions TEXT[],
-    size BIGINT,
+    upload TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT TIMEZONE('UTC', now()),
+    "size" BIGINT,
     bucket TEXT NOT NULL,
     filename TEXT NOT NULL,
     path TEXT NOT NULL,
     status TEXT NOT NULL DEFAULT 'EXPECTED',
-    FOREIGN KEY (owner) REFERENCES users.accounts (id),
+    FOREIGN KEY ("owner") REFERENCES users.accounts ("id"),
     FOREIGN KEY (bot) REFERENCES users.bots (client)
 );
 
-CREATE FUNCTION attachments.update_occupied()
-RETURNS TRIGGER AS
-$$
-BEGIN
-    UPDATE servers.files
-    SET occupied = (
-        SELECT SUM(size)
-        FROM attachments.files
-        WHERE
-            status = 'STORED' AND
-            storage = NEW.storage
-    )
-    WHERE
-        id = NEW.storage;
-    RETURN NEW;
-END;
-$$
-LANGUAGE plpgsql;
-
-CREATE TRIGGER update_occupied
-AFTER INSERT OR UPDATE ON attachments.files
-FOR EACH ROW
-EXECUTE FUNCTION attachments.update_occupied();
-
 CREATE TABLE attachments.widgets (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    owner BIGINT NOT NULL,
+    "id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "owner" BIGINT NOT NULL,
     bot BIGINT,
-    created TIMESTAMP NOT NULL DEFAULT now(),
+    created TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT TIMEZONE('UTC', now()),
     intentions TEXT[],
-    data TEXT[] NOT NULL,
+    "data" TEXT[] NOT NULL,
     status TEXT NOT NULL DEFAULT 'EXPECTED',
-    FOREIGN KEY (owner) REFERENCES users.accounts (id),
+    FOREIGN KEY ("owner") REFERENCES users.accounts ("id"),
     FOREIGN KEY (bot) REFERENCES users.bots (client)
 );
