1d5283e052ca57773484bfeb3b3fa49ba62d9a77
mister-proger <84221561+mister-proger@users.noreply.github.com>
2024-01-05 13:28:34 +0300

    Have put the auxiliary data on permissions in a separate table
    A column of the reference has been separate
    A column with a link-describing permissions has been added

--- permissions/setup.sql
+++ permissions/setup.sql
@@ -1,5 +1,11 @@
 CREATE TABLE public.permissions (
     id SMALLINT PRIMARY KEY,
-    reference TEXT NOT NULL,
     default_value BOOLEAN NOT NULL
+)
+
+CREATE TABLE public.permissions_descriptions (
+    id SMALLINT PRIMARY KEY,
+    reference TEXT,
+    description TEXT,
+    FOREIGN KEY (id) REFERENCES public.permissions (id)
 )
