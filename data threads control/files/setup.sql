5384f16a1f24cf2f25f78e51e201d5476c219c37
mister-proger <i@savnil.ru>
2024-01-18 22:22:53 +0300

    Alter table for files index
    Added access level indication column
    Status column is now written as smallint

--- data threads control/files/setup.sql
+++ data threads control/files/setup.sql
@@ -1,13 +1,14 @@
 CREATE TABLE files."index" (
     "id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "owner" BIGINT NOT NULL,
-    bot BIGINT,
-    upload TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT TIMEZONE('UTC', now()),
+    "bot" BIGINT,
+    "uploaded" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT TIMEZONE('UTC', now()),
     "size" BIGINT,
-    bucket TEXT NOT NULL,
-    filename TEXT NOT NULL,
-    path TEXT NOT NULL,
-    status TEXT NOT NULL DEFAULT 'EXPECTED',
+    "bucket" TEXT NOT NULL,
+    "filename" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "access" SMALLINT NOT NULL DEFAULT 0,
+    "status" SMALLINT NOT NULL DEFAULT 0,
     FOREIGN KEY ("owner") REFERENCES users.accounts ("id"),
     FOREIGN KEY (bot) REFERENCES users.bots (client)
 );
